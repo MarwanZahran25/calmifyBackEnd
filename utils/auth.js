2e1fe0a1eb1801f1305e4ab203f35caf2ca2dfca
MarwanZahran25 <marwanzahran07@gmail.com>
2025-06-02 18:24:53 +0300

    gg

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,11 @@
-jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 async function verifyUser(req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = await jwt.verify(token, process.env.SECRET_KEY);
 
     req.user = await decodedToken;
-    console.log(`${user.email} used a resource`);
+    console.log(`${req.user.email} used a resource`);
     next();
   } catch (err) {
     res.status(401).json("Invalid request");
